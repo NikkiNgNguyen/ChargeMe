2bc43a043fdb955ebe9608ddc070106d81551994
scubaastev <scubaastev@gmail.com>
2019-02-13 01:03:02 -0800

    create account updated

--- src/Views/createAccount.js
+++ src/Views/createAccount.js
@@ -4,6 +4,7 @@ export default class createAccount extends Component {
   render(){
     return (
       <div id="createAccount">
+      <div className="container-fluid bg">
       <div className="row">
         <div className="col-lg-6">
           <div className="bs-component">
@@ -16,9 +17,15 @@ export default class createAccount extends Component {
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"placeholder="Enter email"></input>
                     <small id="emailHelp" className="form-text text-muted"> We'll never share your email with anyone else.</small>
                   </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                  </div>
+                  <button type="submit" className="btn btn-success btn-block">Create Account</button>
                 </div>
                 </fieldset>
               </form>
+      </div>
       </div>
       </div>
       </div>
